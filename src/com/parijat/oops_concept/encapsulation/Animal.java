e592b7b3eb56df7b9fb4b474b490571cd22143be
M Parijat Chandra <m.parijat.chandra.de@gmail.com>
2026-04-28 00:27:05 +0200

    java

--- src/com/parijat/oops_concept/encapsulation/Animal.java
+++ src/com/parijat/oops_concept/encapsulation/Animal.java
@@ -1,12 +1,18 @@
 package com.parijat.oops_concept.encapsulation;
 
 public class Animal {
-    String name;
-    int age;
+    // Public -> AnyWhere
+    // Private -> Only this Class
+    // Protected -> Child Class
 
-    public Animal(String name, int age){
+    public String name;
+    private int age;
+    protected int speed;
+
+    public Animal(String name, int age, int speed){
         this.name = name;
         this.age = age;
+        this. speed = speed;
     }
 
     public void eat(){
