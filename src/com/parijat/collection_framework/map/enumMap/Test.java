215f8fff16cc02ef3b428d1e91b4620f0c1af913
M Parijat Chandra <m.parijat.chandra.de@gmail.com>
2026-06-07 23:50:16 +0200

    Hash Table thread safety

--- src/com/parijat/collection_framework/map/enumMap/Test.java
+++ src/com/parijat/collection_framework/map/enumMap/Test.java
@@ -2,6 +2,6 @@ package com.parijat.collection_framework.map.enumMap;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println("He");
     }
 }
